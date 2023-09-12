40211028b4fbf04275bbec401ac3405070584cfd
Endre Vegh <endre.vegh@yahoo.com>
2023-09-12 07:45:58 +0100

    👔 Add mapping from state -> rows.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+mod app;
+mod handlers;
+
 use std::io;
+use std::ops::Deref;
 use std::time::Duration;
 use std::{error::Error, io::Stdout};
 
-use crossterm::event::{self, Event, KeyCode, KeyModifiers};
+use crossterm::event::{self, Event, KeyCode};
 use crossterm::terminal::{disable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen};
 use crossterm::{execute, terminal::enable_raw_mode};
 
@@ -10,12 +14,10 @@ use ratatui::layout::Layout;
 use ratatui::prelude::{Alignment, Constraint, CrosstermBackend, Direction};
 use ratatui::style::{Color, Modifier, Style};
 use ratatui::terminal::Terminal;
-use ratatui::text::{Line, Span};
-use ratatui::widgets::{Block, BorderType, Borders, Cell, Paragraph, Row, Table};
-
-mod app;
+use ratatui::widgets::{Block, BorderType, Borders, Paragraph, Row, Table};
 
 use app::{ActiveBlock, App};
+use handlers::{handle_down, handle_up};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let mut terminal = setup_terminal()?;
@@ -58,89 +60,100 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
 
             let active_block = app.active_block.clone();
 
-            let converted_rows: Vec<Vec<String>> = requests
+            let index = app.selection_index.clone();
+
+            let converted_rows: Vec<(Vec<String>, bool)> = requests
                 .iter()
                 .map(|request| {
                     let uri = request.uri.clone();
                     let method = request.method.clone().to_string();
                     let time = request.time.clone().to_string();
+                    let id = request.id.clone().to_string();
+                    let selected_item = requests.get(index).clone();
 
-                    vec![method, uri, time]
+                    let selected = match selected_item {
+                        Some(item) => {
+                            if item.deref() == request {
+                                true
+                            } else {
+                                false
+                            }
+                        }
+                        None => false,
+                    };
+
+                    (vec![method, uri, time, id], selected)
                 })
                 .collect();
 
-            let requests = Table::new(vec![
-                // Row can be created from simple strings.
-                Row::new(vec![
-                    "Post - 200",
-                    "https://randomdomain.com/randompath",
-                    "1.58s",
-                ]),
-                // You can style the entire row.
-                Row::new(vec!["Row21", "Row22", "Row23"]).style(Style::default().fg(Color::Blue)),
-                // If you need more control over the styling you may need to create Cells directly
-                Row::new(vec![
-                    Cell::from("Row31"),
-                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
-                    Cell::from(Line::from(vec![
-                        Span::raw("Row"),
-                        Span::styled("33", Style::default().fg(Color::Green)),
-                    ])),
-                ]),
-                Row::new(vec![
-                    Cell::from("test"),
-                    Cell::from("Row32").style(Style::default().fg(Color::Yellow)),
-                    Cell::from(Line::from(vec![
-                        Span::raw("Row"),
-                        Span::styled("33", Style::default().fg(Color::Green)),
-                    ])),
+            let default_style = Style::default().fg(Color::White);
+
+            let selected_style = Style::default().fg(Color::Black).bg(Color::LightRed);
+
+            // NOTE: Why iter or map gives back ref?
+            let _mapped_over: Vec<Paragraph> = [Paragraph::new("title")]
+                .iter()
+                .map(|x| {
+                    let cloned = x.deref().clone();
+
+                    cloned
+                })
+                .collect();
+
+            let styled_rows: Vec<Row> = converted_rows
+                .iter()
+                .map(|(row, selected)| {
+                    let str_vec: Vec<&str> = row
+                        .iter()
+                        .map(|x| x.as_str())
+                        .collect::<Vec<&str>>()
+                        .clone();
+
+                    Row::new(str_vec).style(if *selected {
+                        selected_style
+                    } else {
+                        default_style
+                    })
+                })
+                .collect();
+
+            let requests = Table::new(styled_rows)
+                // You can set the style of the entire Table.
+                .style(Style::default().fg(Color::White))
+                // It has an optional header, which is simply a Row always visible at the top.
+                .header(
+                    Row::new(vec!["Method", "Request", "Time"])
+                        .style(Style::default().fg(Color::Yellow))
+                        // If you want some space between the header and the rest of the rows, you can always
+                        // specify some margin at the bottom.
+                        .bottom_margin(1),
+                )
+                // As any other widget, a Table can be wrapped in a Block.
+                .block(
+                    Block::default()
+                        .borders(Borders::ALL)
+                        .style(Style::default().fg(
+                            if active_block == ActiveBlock::NetworkRequests {
+                                Color::White
+                            } else {
+                                Color::DarkGray
+                            },
+                        ))
+                        .title("Network requests")
+                        .border_type(BorderType::Plain),
+                )
+                // Columns widths are constrained in the same way as Layout...
+                .widths(&[
+                    Constraint::Percentage(10),
+                    Constraint::Percentage(70),
+                    Constraint::Length(20),
                 ])
-                .style(Style::default().fg(Color::White).bg(Color::Magenta)),
-                // If a Row need to display some content over multiple lines, you just have to change
-                // its height.
-                Row::new(vec![
-                    Cell::from("Row\n41"),
-                    Cell::from("Row\n42"),
-                    Cell::from("Row\n43"),
-                ])
-                .height(2),
-            ])
-            // You can set the style of the entire Table.
-            .style(Style::default().fg(Color::White))
-            // It has an optional header, which is simply a Row always visible at the top.
-            .header(
-                Row::new(vec!["Method", "Request", "Time"])
-                    .style(Style::default().fg(Color::Yellow))
-                    // If you want some space between the header and the rest of the rows, you can always
-                    // specify some margin at the bottom.
-                    .bottom_margin(1),
-            )
-            // As any other widget, a Table can be wrapped in a Block.
-            .block(
-                Block::default()
-                    .borders(Borders::ALL)
-                    .style(
-                        Style::default().fg(if active_block == ActiveBlock::NetworkRequests {
-                            Color::White
-                        } else {
-                            Color::DarkGray
-                        }),
-                    )
-                    .title("Network requests")
-                    .border_type(BorderType::Plain),
-            )
-            // Columns widths are constrained in the same way as Layout...
-            .widths(&[
-                Constraint::Percentage(10),
-                Constraint::Percentage(70),
-                Constraint::Length(20),
-            ])
-            // ...and they can be separated by a fixed spacing.
-            // .column_spacing(1)
-            // If you wish to highlight a row in any specific way when it is selected...
-            .highlight_style(Style::default().add_modifier(Modifier::BOLD))
-            // ...and potentially show a symbol in front of the selection.
-            .highlight_symbol(">>");
+                // ...and they can be separated by a fixed spacing.
+                // .column_spacing(1)
+                // If you wish to highlight a row in any specific way when it is selected...
+                .highlight_style(Style::default().add_modifier(Modifier::BOLD))
+                // ...and potentially show a symbol in front of the selection.
+                .highlight_symbol(">>");
 
             let details = Paragraph::new("Request details")
                 .style(
@@ -165,29 +178,6 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                         .border_type(BorderType::Plain),
                 );
 
-            // let requests = Paragraph::new("Requests")
-            //     .style(
-            //         Style::default().fg(if active_block == ActiveBlock::NetworkRequests {
-            //             Color::White
-            //         } else {
-            //             Color::DarkGray
-            //         }),
-            //     )
-            //     .alignment(Alignment::Center)
-            //     .block(
-            //         Block::default()
-            //             .borders(Borders::ALL)
-            //             .style(Style::default().fg(
-            //                 if active_block == ActiveBlock::NetworkRequests {
-            //                     Color::White
-            //                 } else {
-            //                     Color::DarkGray
-            //                 },
-            //             ))
-            //             .title("Network requests")
-            //             .border_type(BorderType::Plain),
-            //     );
-
             frame.render_widget(requests, main_layout[0]);
 
             frame.render_widget(details, main_layout[1]);
@@ -199,28 +189,10 @@ fn run(terminal: &mut Terminal<CrosstermBackend<Stdout>>) -> Result<(), Box<dyn 
                         break;
                     }
                     KeyCode::Char('k') => {
-                        if app.active_block == ActiveBlock::RequestDetails {
-                            app.active_block = ActiveBlock::NetworkRequests
-                        } else {
-                            app.active_block = ActiveBlock::RequestDetails
-                        }
+                        handle_up(&mut app, key);
                     }
                     KeyCode::Char('j') => {
-                        if key.modifiers == KeyModifiers::CONTROL {}
-                        if app.active_block == ActiveBlock::RequestDetails {
-                            app.active_block = ActiveBlock::NetworkRequests
-                        } else {
-                            app.active_block = ActiveBlock::RequestDetails
-                        }
-                    }
-
-                    KeyCode::Tab => {
-                        // TODO: Figure out a way to make ModifierKeyCode work here.
-                        if app.active_block == ActiveBlock::RequestDetails {
-                            app.active_block = ActiveBlock::NetworkRequests
-                        } else {
-                            app.active_block = ActiveBlock::RequestDetails
-                        }
+                        handle_down(&mut app, key);
                     }
                     _ => {}
                 }
