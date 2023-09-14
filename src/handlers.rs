e49f214044aa69c11b3f016ece59e7e306fac569
Endre Vegh <endre.vegh@yahoo.com>
2023-09-14 20:42:33 +0100

    🐛 Update handlers to fix navigation bug.

--- src/handlers.rs
+++ src/handlers.rs
@@ -1,11 +1,14 @@
 use crossterm::event::{KeyEvent, KeyModifiers};
 
 use crate::app::{ActiveBlock, App};
+use crate::utils::parse_query_params;
 
 pub fn handle_up(app: &mut App, key: KeyEvent) {
     match key.modifiers {
         KeyModifiers::CONTROL => match app.active_block {
             ActiveBlock::RequestHeaders => app.active_block = ActiveBlock::RequestDetails,
+            ActiveBlock::ResponseHeaders => app.active_block = ActiveBlock::RequestHeaders,
+            ActiveBlock::RequestDetails => app.active_block = ActiveBlock::Summary,
             _ => {}
         },
         _ => match app.active_block {
@@ -13,16 +16,30 @@ pub fn handle_up(app: &mut App, key: KeyEvent) {
                 if app.selection_index > 0 {
                     app.selection_index = app.selection_index - 1;
                 }
+
+                app.selected_params_index = 0
+            }
+            ActiveBlock::RequestDetails => {
+                let next_index = if app.selected_params_index == 0 {
+                    0
+                } else {
+                    app.selected_params_index - 1
+                };
+
+                app.selected_params_index = next_index
             }
             _ => {}
         },
     }
 }
 
+// NOTE: Find stg like urlsearchparams for JS.
 pub fn handle_down(app: &mut App, key: KeyEvent) {
     match key.modifiers {
         KeyModifiers::CONTROL => match app.active_block {
+            ActiveBlock::Summary => app.active_block = ActiveBlock::RequestDetails,
             ActiveBlock::RequestDetails => app.active_block = ActiveBlock::RequestHeaders,
+            ActiveBlock::RequestHeaders => app.active_block = ActiveBlock::ResponseHeaders,
             _ => {}
         },
         _ => match app.active_block {
@@ -32,6 +49,21 @@ pub fn handle_down(app: &mut App, key: KeyEvent) {
                 if app.selection_index + 1 < length {
                     app.selection_index = app.selection_index + 1;
                 }
+
+                app.selected_params_index = 0
+            }
+            ActiveBlock::RequestDetails => {
+                let index = &app.items[app.selection_index];
+
+                let params = parse_query_params(index.uri.clone());
+
+                let next_index = if app.selected_params_index + 1 >= params.len() {
+                    params.len() - 1
+                } else {
+                    app.selected_params_index + 1
+                };
+
+                app.selected_params_index = next_index
             }
             _ => {}
         },
@@ -54,7 +86,7 @@ pub fn handle_right(app: &mut App, _key: KeyEvent) {
 
 pub fn handle_enter(app: &mut App, _key: KeyEvent) {
     if app.active_block == ActiveBlock::NetworkRequests {
-        app.active_block = ActiveBlock::RequestDetails
+        app.active_block = ActiveBlock::Summary
     }
 }
 
