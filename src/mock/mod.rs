004ee31001c4108d28e03452263df2ad149152b4
Endre Vegh <endre.vegh@yahoo.com>
2023-10-10 06:03:22 +0100

    🤡 Refactor mocks to new structure.

--- src/mock/mod.rs
+++ src/mock/mod.rs
@@ -104,18 +104,89 @@ pub const TEST_JSON_10: &str = r#"{
 
 }"#;
 
-pub const TEST_JSON_11: &str = r#"{"id":"11","type":"HttpRequest","timestamp":1694948911168,"httpVersion":"1.1","method":"POST","host":"localhost","port":3000,"path":"/api/graphql","url":"http://localhost:3000/api/graphql","requestHeaders":{"authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.vqb33-7FqzFWPNlr0ElW1v2RjJRZBel3CdDHBWD7y_o","content-type":["application/json"],"Accept":["*/*"],"User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"Accept-Encoding":["gzip,deflate"]},"requestBody":"{\"query\":\"query People {\\n  people {\\n    id\\nfirstName\\nlastName\\n  }\\n}\\n\",\"operationName\":\"People\",\"variables\":{}}","statusCode":200,"statusMessage":"OK","responseHeaders":{"x-powered-by":"Express","cache-control":"private, no-store","surrogate-key":"all","access-control-allow-origin":"*","access-control-allow-credentials":"true","content-type":"application/json","content-length":"28","vary":"Accept-Encoding","date":"Thu, 17 Mar 2022 19:51:01 GMT","connection":"keep-alive","keep-alive":"timeout=5"},"responseBody":"{\"data\":{\"people\":[{\"id\":\"1\",\"firstName\":\"Peter\",\"lastName\":\"Piper\"},{\"id\":\"2\",\"firstName\":\"Tom\",\"lastName\":\"Thumbdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd\"},{\"id\":\"3\",\"firstName\":\"Mary\",\"lastName\":\"Hadalittlelamb\"}]}}","duration":500}"#;
+pub const TEST_JSON_11: &str = r#"{
 
-pub const TEST_JSON_12: &str = r#"{"id":"12","type":"HttpRequest","timestamp":1694943912369,"httpVersion":"1.1","method":"GET","host":"data.restserver.com","port":443,"path":"/features","url":"http://data.restserver.com/features","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"},"statusCode":200,"statusMessage":"OK","responseHeaders":{"content-type":"application/json; charset=utf-8","content-length":"351","date":"Thu, 17 Mar 2022 19:51:00 GMT","vary":"Origin","connection":"close"},"responseBody":"{\"awesomeFeature\":true,\"crappyFeature\":false}","duration":15}"#;
 
-pub const TEST_JSON_13: &str = r#"{"id":"13","type":"HttpRequest","timestamp":1694947915469,"httpVersion":"1.1","method":"GET","host":"data.restserver.com","port":443,"path":"/countries?start=0&count=20","url":"http://data.restserver.com/countries?start=0&count=20","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"},"statusCode":404,"statusMessage":"Not found","responseHeaders":{"content-type":"application/json; charset=utf-8","content-length":"2","date":"Thu, 17 Mar 2022 19:51:01 GMT","vary":"Origin","connection":"close"},"duration":10}"#;
+    "data": {
+"id":"11","type":"HttpRequest","timestamp":1694948911168,"http": {"httpVersion":"1.1","method":"POST","host":"localhost","port":3000,"path":"/api/graphql","url":"http://localhost:3000/api/graphql","requestHeaders":{"authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.vqb33-7FqzFWPNlr0ElW1v2RjJRZBel3CdDHBWD7y_o","content-type":["application/json"],"Accept":["*/*"],"User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"Accept-Encoding":["gzip,deflate"]},"requestBody":"{\"query\":\"query People {\\n  people {\\n    id\\nfirstName\\nlastName\\n  }\\n}\\n\",\"operationName\":\"People\",\"variables\":{}}","statusCode":200,"statusMessage":"OK","responseHeaders":{"x-powered-by":"Express","cache-control":"private, no-store","surrogate-key":"all","access-control-allow-origin":"*","access-control-allow-credentials":"true","content-type":"application/json","content-length":"28","vary":"Accept-Encoding","date":"Thu, 17 Mar 2022 19:51:01 GMT","connection":"keep-alive","keep-alive":"timeout=5"},"responseBody":"{\"data\":{\"people\":[{\"id\":\"1\",\"firstName\":\"Peter\",\"lastName\":\"Piper\"},{\"id\":\"2\",\"firstName\":\"Tom\",\"lastName\":\"Thumbdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd\"},{\"id\":\"3\",\"firstName\":\"Mary\",\"lastName\":\"Hadalittlelamb\"}]}}","duration":500
+}    }
 
-pub const TEST_JSON_14: &str = r#"{"id":"14","type":"HttpRequest","timestamp":1394948931769,"httpVersion":"1.1","method":"POST","host":"data.restserver.com","port":443,"path":"/people","url":"http://data.restserver.com/people","requestHeaders":{"authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.vqb33-7FqzFWPNlr0ElW1v2RjJRZBel3CdDHBWD7y_o","content-type":["application/json"],"Accept":["*/*"],"User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"Accept-Encoding":["gzip,deflate"]},"requestBody":"{\"firstName\":\"Paddington\",\"lastName\":\"Bear\"}","statusCode":200,"statusMessage":"OK","responseHeaders":{"cache-control":"private, no-store","surrogate-key":"all","access-control-allow-origin":"*","access-control-allow-credentials":"true","content-type":"application/json","content-length":"11","vary":"Accept-Encoding","date":"Thu, 17 Mar 2022 19:51:02 GMT","connection":"keep-alive","keep-alive":"timeout=5"},"responseBody":"{\"id\":\"4\"}","duration":1300}"#;
+}"#;
 
-pub const TEST_JSON_15: &str = r#"{"id":"15","type":"HttpRequest","timestamp":1694448931869,"httpVersion":"1.1","method":"POST","host":"localhost","port":3000,"path":"/api/graphql","url":"http://localhost:3000/api/graphql","requestHeaders":{"authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.vqb33-7FqzFWPNlr0ElW1v2RjJRZBel3CdDHBWD7y_o","content-type":["application/json"],"Accept":["*/*"],"User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"Accept-Encoding":["gzip,deflate"]},"requestBody":"{\"query\":\"mutation RegisterPerson($id: String) {\\n  registerPerson(id: $id) {\\n    success\\n}\\n}\",\"type\":\"HttpRequest\",\"operationName\":\"RegisterPerson\",\"variables\":{\"id\":\"4\",\"type\":\"HttpRequest\"}}","statusCode":200,"statusMessage":"OK","responseHeaders":{"x-powered-by":"Express","cache-control":"private, no-store","surrogate-key":"all","access-control-allow-origin":"*","access-control-allow-credentials":"true","content-type":"application/json","content-length":"28","vary":"Accept-Encoding","date":"Thu, 17 Mar 2022 19:51:01 GMT","connection":"keep-alive","keep-alive":"timeout=5"},"responseBody":"{\"data\":{\"success\":true}}","duration":629}"#;
+pub const TEST_JSON_12: &str = r#"{
 
-pub const TEST_JSON_16: &str = r#"{"id":"16","type":"HttpRequest","timestamp":1694948935009,"httpVersion":"1.1","method":"GET","host":"data.restserver.com","port":433,"path":"/movies?start=0&count=20","url":"https://data.restserver.com:433/movies?start=0&count=20","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"},"statusCode":500,"statusMessage":"Internal Server Error","responseHeaders":{"content-type":"application/json; charset=utf-8","content-length":"0","date":"Thu, 17 Mar 2022 19:51:01 GMT","vary":"Origin","connection":"close"},"duration":5000}"#;
 
-pub const TEST_JSON_17: &str = r#"{"id":"17","type":"HttpRequest","timestamp":1624948938149,"httpVersion":"1.1","method":"GET","host":"hits.webstats.com","port":433,"path":"/?apikey=c82e66bd-4d5b-4bb7-b439-896936c94eb2","url":"https://hits.webstats.com:433/?apikey=c82e66bd-4d5b-4bb7-b439-896936c94eb2","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"},"statusCode":200,"statusMessage":"OK","responseHeaders":{"content-type":"application/xml; charset=utf-8","content-length":"55","date":"Thu, 17 Mar 2022 19:51:01 GMT","vary":"Origin","connection":"close"},"responseBody":"<hits><today>10</today><yesterday>15</yesterday></hits>","duration":5000}"#;
+    "data": {
+"id":"12","type":"HttpRequest","timestamp":1694943912369, "http": {
+"httpVersion":"1.1","method":"GET","host":"data.restserver.com","port":443,"path":"/features","url":"http://data.restserver.com/features","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"},"statusCode":200,"statusMessage":"OK","responseHeaders":{"content-type":"application/json; charset=utf-8","content-length":"351","date":"Thu, 17 Mar 2022 19:51:00 GMT","vary":"Origin","connection":"close"},"responseBody":"{\"awesomeFeature\":true,\"crappyFeature\":false}","duration":15
 
-pub const TEST_JSON_18: &str = r#"{"id":"18","type":"HttpRequest","timestamp":1694948938539,"httpVersion":"1.1","method":"GET","host":"data.restserver.com","port":433,"path":"/features","url":"https://data.restserver.com:433/features","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"}}"#;
+}    }
+
+
+}"#;
+//
+
+pub const TEST_JSON_13: &str = r#"{ 
+
+"data": {
+"id":"13","type":"HttpRequest","timestamp":1694947915469,
+
+"http": {
+"httpVersion":"1.1","method":"GET","host":"data.restserver.com","port":443,"path":"/countries?start=0&count=20","url":"http://data.restserver.com/countries?start=0&count=20","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"},"statusCode":404,"statusMessage":"Not found","responseHeaders":{"content-type":"application/json; charset=utf-8","content-length":"2","date":"Thu, 17 Mar 2022 19:51:01 GMT","vary":"Origin","connection":"close"},"duration":10
+
+}
+
+}
+
+}"#;
+
+pub const TEST_JSON_14: &str = r#"{
+
+    "data": {
+"id":"14","type":"HttpRequest","timestamp":1394948931769, "http" :{
+"httpVersion":"1.1","method":"POST","host":"data.restserver.com","port":443,"path":"/people","url":"http://data.restserver.com/people","requestHeaders":{"authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.vqb33-7FqzFWPNlr0ElW1v2RjJRZBel3CdDHBWD7y_o","content-type":["application/json"],"Accept":["*/*"],"User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"Accept-Encoding":["gzip,deflate"]},"requestBody":"{\"firstName\":\"Paddington\",\"lastName\":\"Bear\"}","statusCode":200,"statusMessage":"OK","responseHeaders":{"cache-control":"private, no-store","surrogate-key":"all","access-control-allow-origin":"*","access-control-allow-credentials":"true","content-type":"application/json","content-length":"11","vary":"Accept-Encoding","date":"Thu, 17 Mar 2022 19:51:02 GMT","connection":"keep-alive","keep-alive":"timeout=5"},"responseBody":"{\"id\":\"4\"}","duration":1300
+
+}    }
+
+}"#;
+
+pub const TEST_JSON_15: &str = r#"{
+
+    "data": {
+"id":"15","type":"HttpRequest","timestamp":1694448931869,"http": {
+"httpVersion":"1.1","method":"POST","host":"localhost","port":3000,"path":"/api/graphql","url":"http://localhost:3000/api/graphql","requestHeaders":{"authorization":"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.vqb33-7FqzFWPNlr0ElW1v2RjJRZBel3CdDHBWD7y_o","content-type":["application/json"],"Accept":["*/*"],"User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"Accept-Encoding":["gzip,deflate"]},"requestBody":"{\"query\":\"mutation RegisterPerson($id: String) {\\n  registerPerson(id: $id) {\\n    success\\n}\\n}\",\"type\":\"HttpRequest\",\"operationName\":\"RegisterPerson\",\"variables\":{\"id\":\"4\",\"type\":\"HttpRequest\"}}","statusCode":200,"statusMessage":"OK","responseHeaders":{"x-powered-by":"Express","cache-control":"private, no-store","surrogate-key":"all","access-control-allow-origin":"*","access-control-allow-credentials":"true","content-type":"application/json","content-length":"28","vary":"Accept-Encoding","date":"Thu, 17 Mar 2022 19:51:01 GMT","connection":"keep-alive","keep-alive":"timeout=5"},"responseBody":"{\"data\":{\"success\":true}}","duration":629
+}
+    }
+}"#;
+
+pub const TEST_JSON_16: &str = r#"{
+
+
+
+    "data": {
+"id":"16","type":"HttpRequest","timestamp":1694948935009, "http": {
+"httpVersion":"1.1","method":"GET","host":"data.restserver.com","port":433,"path":"/movies?start=0&count=20","url":"https://data.restserver.com:433/movies?start=0&count=20","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"},"statusCode":500,"statusMessage":"Internal Server Error","responseHeaders":{"content-type":"application/json; charset=utf-8","content-length":"0","date":"Thu, 17 Mar 2022 19:51:01 GMT","vary":"Origin","connection":"close"},"duration":5000
+
+}    }
+
+}"#;
+
+pub const TEST_JSON_17: &str = r#"{
+
+
+    "data": {
+"id":"17","type":"HttpRequest","timestamp":1624948938149,"http": {
+"httpVersion":"1.1","method":"GET","host":"hits.webstats.com","port":433,"path":"/?apikey=c82e66bd-4d5b-4bb7-b439-896936c94eb2","url":"https://hits.webstats.com:433/?apikey=c82e66bd-4d5b-4bb7-b439-896936c94eb2","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"},"statusCode":200,"statusMessage":"OK","responseHeaders":{"content-type":"application/xml; charset=utf-8","content-length":"55","date":"Thu, 17 Mar 2022 19:51:01 GMT","vary":"Origin","connection":"close"},"responseBody":"<hits><today>10</today><yesterday>15</yesterday></hits>","duration":5000
+
+}    }
+
+}"#;
+
+pub const TEST_JSON_18: &str = r#"{
+    "data": {
+"id":"18","type":"HttpRequest","timestamp":1694948938539,"http": {
+"httpVersion":"1.1","method":"GET","host":"data.restserver.com","port":433,"path":"/features","url":"https://data.restserver.com:433/features","requestHeaders":{"accept":"application/json","User-Agent":["node-fetch/1.0 (+https://github.com/bitinn/node-fetch)"],"accept-encoding":"br, gzip, deflate"}
+}
+    }
+
+}"#;
